1e1591e1eee000cbd6a5ca3c3ba85c827256a27a
gboned <gboned@cifpfbmoll.eu>
2020-05-22 19:22:17 +0200

    Crear función para generar los coches para dueños

--- js/funciones.js
+++ js/funciones.js
@@ -17,3 +17,17 @@ function coche(marcasCoches, duenyosCoches) {
     this.marcasCoches = marcasCoches;
     this.duenyosCoches = duenyosCoches
 }
+
+// Creo la función para generar los coches, las marcas se pueden repetir, pero los dueños no.
+function generadorCoches(){
+    // Creo un bucle for, para que se vayan generando coches, y mientras i sea menor a la longitud de la variable, sumará 1 (y se añadirá un nuevo coche).
+    for (i = 0; i < marcasCoches.length; i++) {
+        // Creo una variable que instancia nuevos coches, para sacar la marca de coche, muestra la marca de una posición aleatoria, y para
+        // mostrar el dueño del coche, selecciona el dueño en la primera posición.
+        var coches = new coche(marcasCoches[(Math.floor(Math.random() * marcasCoches.length))], duenyosCoches[0]);
+        // Cada vez que se crea un nuevo coche, se elimina el primer dueño de la variable, así el duenyosCoches[0] irá sacando un coche nuevo cada vez.
+        duenyosCoches.shift();
+        // Muestro los coches por consola.
+        console.log(coches);
+    }
+}
