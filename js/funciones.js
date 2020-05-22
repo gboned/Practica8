4d99fe79e167a5a36d4f307cf766b2dda59c9235
gboned <gboned@cifpfbmoll.eu>
2020-05-22 19:21:28 +0200

    Crear dos variables con arrays de marcas y dueños

--- js/funciones.js
+++ js/funciones.js
@@ -3,3 +3,11 @@
 /*  Construir Objetos Automáticamente   */
 /*                                      */
 /****************************************/
+
+// Una empresa mandará construir 10 coches para 10 de sus empleados.
+
+// Creo una variable con las 10 posibles marcas de coche (puede que alguna de ellas no se utilice).
+var marcasCoches = ["Ferrari", "Fiat", "Smart", "Porsche", "Citroen", "Tesla", "Toyota", "Opel", "Audi", "Lancia"];
+
+// Creo una variable con los nombres de los 10 empleados que recibirán un coche.
+var duenyosCoches = ["David", "Brian", "Laura", "Cristian", "Lola", "Estefanía", "Cristina", "Manuel", "Leonardo", "Ana"];
